7c1ed4b6d0fdbd98718a9c5b3c5afd8a15e67227
Bogdan Yakupov <yourinfinityuser@gmail.com>
2024-03-11 21:53:50 +0500

    phone number title box added

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 Membership card generation script
 
 ## Usage
-Type: `$> python make_card.py <template path> <output image path> "full name"` 
+Type: `$> python make_card.py <template path> <output image path> "full name" "phone number"` 
 
 Example:
 
-`$> python make_card.py ./resources/template.jpeg ./out.jpeg "иванов юлий рустамович"` 
+`$> python make_card.py ./resources/template.jpeg ./resources/example.png "иванов илья соматович" "+77777777777"` 
 
-Creates a finalized membership card image `./out.jpeg`
+Creates a finalized membership card image `./resources/example.png`
 
-![image](./resources/example.jpeg)
+![image](./resources/example.png)
--- make_card.py
+++ make_card.py
@@ -6,11 +6,18 @@ font_type = ImageFont.truetype(Config.FONT_FACE, Config.FONT_SIZE)
 
 if __name__=='__main__':
 	if len(sys.argv) < 3:
-		print('Run with a template file and an input string')
+		print('Pass a template file and an input string')
 		exit()
 
 	template = Image.open(sys.argv[1])
 	full_name = [i.strip().upper() for i in sys.argv[3].split(' ')]
+	phone = [i for i in sys.argv[4] if i in '0123456789']
+	try:
+		phone = f'+7-({phone[-10]}{phone[-9]}{phone[-8]})-{phone[-7]}{phone[-6]}{phone[-5]}-{phone[-4]}{phone[-3]}-{phone[-2]}{phone[-1]}'
+	except:
+		print('Pass a right phone number specified')
+		exit()
+		
 	ln = max([len(i) for i in full_name])
 	
 	drawer = ImageDraw.Draw(template)
@@ -20,5 +27,8 @@ if __name__=='__main__':
 	for word in full_name:
 		drawer.text(orientation, word, font=font_type, fill=Config.TEXT_COLOR_NORMAL, stroke_width=Config.FONT_WIDTH_NORMAL)
 		orientation[1] += Config.TEXT_SPACING
+	
+	orientation = Config.PHONE_ORIENTATION
+	drawer.text(orientation, phone, font=font_type, fill=Config.TEXT_COLOR_NORMAL, stroke_width=Config.FONT_WIDTH_NORMAL)
 	
 	template.save(sys.argv[2])
